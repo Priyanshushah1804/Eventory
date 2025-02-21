540cecb4c82ded106489f8f62d49695eef44169f
datmedevil17 <rakshitshukla17@gmail.com>
2025-02-22 02:20:55 +0530

    fetch Profile

--- client/src/pages/Demo.jsx
+++ client/src/pages/Demo.jsx
@@ -4,7 +4,7 @@ import { ethers } from 'ethers';
 const Demo = ({state,account}) => {
     const {contract} = state;
     const buyTicket = async()=>{
-        const tx = await contract.mint(1, 99, { value: ethers.parseEther("0.00005") });
+        const tx = await contract.mint(1, 15, { value: ethers.parseEther("0.00005") });
         await tx.wait();
         console.log(tx)
     }
@@ -15,10 +15,15 @@ const Demo = ({state,account}) => {
     const scanTicket = async()=>{
         const tx = await contract.scanTicket(1)
     }
+    const accessProfile = async()=>{
+        const tx = await contract.getUserTickets(account)
+        console.log(tx)
+    }
   return (
     <div>
         <button onClick={buyTicket}>Buy</button>
-        <button onClick={getSeatsTaken}>Get</button>
+        <button onClick={getSeatsTaken}>Get</button>\
+        <button onClick={accessProfile}>Access</button>
 
     </div>
   )
